073cf4685ea91bed1d5753fb7c750d7d0ef13cbe
danilo992 <tadeudanilo46@gmail.com>
2019-10-03 11:59:06 -0300

    projeto node.js, React e React Native.

---
+++ backend/src/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-au39i.mongodb.net/semana09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+//GET, POST, PUT, DELETE
+
+//POST rer.quey = Acessar query params
+//PUT req.params = Acessar rout params (para edição, delete)
+//POST => JSON req.body = Acessar corpo da requisição (para criação, edição)
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
