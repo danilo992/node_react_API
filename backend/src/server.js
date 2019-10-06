c5adefef7f681437ab42f054ce39f178ae81f7db
danilo992 <tadeudanilo46@gmail.com>
2019-10-06 02:16:38 -0300

    add fronend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const routes = require('./routes');
 
@@ -16,6 +17,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-au39i.mongodb.net/s
 //PUT req.params = Acessar rout params (para edição, delete)
 //POST => JSON req.body = Acessar corpo da requisição (para criação, edição)
 
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 
